3fa17a50065cb13ced98f7b747a879795d28bacb
Md Javedul Ferdous <mdjavedulferdous@gmail.com>
2021-08-14 18:46:11 -0400

    video

--- YouTilt/app/src/main/java/com/example/YouTilt/MainActivity.java
+++ YouTilt/app/src/main/java/com/example/YouTilt/MainActivity.java
@@ -14,8 +14,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 import java.lang.*;
 import java.util.ArrayList;
-import java.util.List;
-
+import android.content.Context;
+import android.hardware.SensorEventListener;
+import java.util.Objects;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -30,16 +31,23 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
     private Accelerometer accelerometer;
     public TextView txtView, txtview2;
     float rightValue=0;
-    private SensorManager flickManager;
-    private Sensor flick;
-    private float[] mGravity;
+    int max = 0, i =0;
+    private SensorManager shakeManager;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
     private Gyroscope gyroscope;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        shakeManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Objects.requireNonNull(shakeManager).registerListener(mSensorListener, shakeManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        mAccel = 10f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
 
         youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
         youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
@@ -64,192 +72,17 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
 
                 if (ry > 1.0f)
                 {
-                    /*getWindow().getDecorView().setBackgroundColor(Color.RED);
-                    Log.d("TX Value:"+ tx,"Right");
-                    rightValue = (int)tx * 10000;
-                    rightValue +=player.getCurrentTimeMillis();
-                    player.seekToMillis(Math.abs((rightValue)));
-                    txtView.setText("Forward:"+(int)(rightValue/10000));
-                    rightValue=0;*/
-                        getWindow().getDecorView().setBackgroundColor(Color.RED);
-                        //rightValue = tx;
-                        //rightValue +=player.getCurrentTimeMillis();
-                        //Log.d("TX Value:"+ tx+"RightValue:"+rightValue,"Right");
-                        Log.v("Right First","Ry "+ry);
+                        getWindow().getDecorView().setBackgroundColor(Color.DKGRAY);
+                        player.seekRelativeMillis (10000);
+                        txtView.setText("Forward 10 second");
 
-                        if(ry >1.0 &&  ry <=1.2)
-                        {
-                            player.seekRelativeMillis (10000);
-                            txtView.setText("Forward 1.5x");
-                            rightValue=0;
-                            Log.v("Right","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else if(ry >1.20 &&  ry <=1.30)
-                        {
-                            player.seekRelativeMillis (20000);
-                            txtView.setText("Forward 2x");
-                            rightValue=0;
-                            Log.v("Right","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-
-                        }
-                        else if(ry >1.30 &&  ry <=1.40)
-                        {
-                            player.seekRelativeMillis (30000);
-                            txtView.setText("Forward 3x");
-                            rightValue=0;
-                            Log.v("Right","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else if(ry >1.40 && ry <=1.5)
-                        {
-                            player.seekRelativeMillis (40000);
-                            txtView.setText("Forward 4x");
-                            rightValue=0;
-                            Log.v("Right","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else if(ry >1.5 &&  ry <=1.7)
-                        {
-                            player.seekRelativeMillis (50000);
-                            txtView.setText("Forward 5x");
-                            rightValue=0;
-                            Log.v("Right","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else if(ry >1.7 &&  ry <=1.9)
-                        {
-                            player.seekRelativeMillis (60000);
-                            txtView.setText("Forward 6x");
-                            rightValue=0;
-                            Log.v("Right","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else {
-                            player.seekRelativeMillis (70000);
-                            txtView.setText("Forward 7x and more");
-                            rightValue=0;
-                            Log.v("Right","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        //txtView.setText("Forward:"+(int)(rightValue/10000));
-                        //rightValue=0;
-                    }
+                }
                     else if (ry < -1.0f) {
-                    /*getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                    rightValue = Math.abs((int)tx * 10000);
-                    rightValue -=player.getCurrentTimeMillis();
-                    player.seekToMillis(Math.abs((rightValue)));
-                    Log.d("TX Value:"+ tx,"Left");
-                    txtView.setText("Backward:"+Math.abs((int)(rightValue/10000)));
-                    rightValue=0;*/
                         getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                        //rightValue = tx;
-                        //rightValue +=player.getCurrentTimeMillis();
-                        //Log.d("TX Value:"+ tx+"RightValue:"+rightValue,"Right");
-                        Log.v("Left First","ry "+ry);
-
-                        if(ry <-1.0 &&  ry >=-1.2)
-                        {
-                            player.seekRelativeMillis (-10000);
-                            txtView.setText("Backward 1x");
-                            rightValue=0;
-                            Log.v("left","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else if(ry <-1.20 &&  ry >=-1.30)
-                        {
-                            player.seekRelativeMillis (-20000);
-                            txtView.setText("Backward 2x");
-                            rightValue=0;
-                            Log.v("left","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else if(ry <-1.30 &&  ry >=-1.40)
-                        {
-                            player.seekRelativeMillis (-30000);
-                            txtView.setText("Backward 3x");
-                            rightValue=0;
-                            Log.v("left","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else if(ry <-1.40 && ry >=-1.5)
-                        {
-                            player.seekRelativeMillis (-40000);
-                            txtView.setText("Backward 4x");
-                            rightValue=0;
-                            Log.v("left","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else if(ry <-1.5 &&  ry >=-1.7)
-                        {
-                            player.seekRelativeMillis (-50000);
-                            txtView.setText("Backward 5x");
-                            rightValue=0;
-                            Log.v("left","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else if(ry <-1.7 &&  ry >=-1.9)
-                        {
-                            player.seekRelativeMillis (-60000);
-                            txtView.setText("Backward 6x");
-                            rightValue=0;
-                            Log.v("left","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
-                        else {
-                            player.seekRelativeMillis (-70000);
-                            txtView.setText("Backward 7x and more");
-                            rightValue=0;
-                            Log.v("left","ry "+ry);
-                            txtview2.setText("Ry: " + ry);
-                            rx = 0;
-                            ry = 0;
-                            rz = 0;
-                        }
+                        player.seekRelativeMillis(-10000);
+                        txtView.setText("Backward 10 second");
                     }
                 }
-
         });
 
         accelerometer = new Accelerometer(this);
@@ -258,17 +91,17 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
             public void onTranslation(float tx, float ty, float tz) {
                 if (tx > 1.0f)
                 {
-                    try {
+                    /*try {
                         player.next();
                         txtView.setText("Next");
                         getWindow().getDecorView().setBackgroundColor(Color.MAGENTA);
                     } catch (Exception e) {
                         txtView.setText("No video is available");
-                    }
+                    }*/
                 }
                 else if(tx <- 1.0f)
                 {
-                    try{
+                    /*try{
                         player.previous();
                         txtView.setText("Previous");
                         getWindow().getDecorView().setBackgroundColor(Color.CYAN);
@@ -276,33 +109,68 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
                     catch (Exception e)
                     {
                         txtView.setText("No video is available");
-                    }
+                    }*/
 
                 }
                 if (ty > 1.0f)
                 {
                     getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-                    player.pause();
+                    /*player.pause();
                     Log.v("Left","Tx "+tx);
-                    txtView.setText("Pause");
+                    txtView.setText("Pause");*/
 
                 }
                 else if (ty < -1.0f)
                 {
                     getWindow().getDecorView().setBackgroundColor(Color.DKGRAY);
-                    Log.d("TY Value:"+ ty,"DOWN");
+                    /*Log.d("TY Value:"+ ty,"DOWN");
                     player.play();
-                    txtView.setText("Play");
+                    txtView.setText("Play");*/
                 }
             }
         });
     }
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            if (mAccel > 12) {
+                //Toast.makeText(getApplicationContext(), "Shake event detected", Toast.LENGTH_SHORT).show();
+                if(player.isPlaying() == true)
+                {
+                    player.pause();
+                    txtView.setText("Pause");
+                    getWindow().getDecorView().setBackgroundColor(Color.CYAN);
+                }
+                else{
+                    player.play();
+                    txtView.setText("Play");
+                    getWindow().getDecorView().setBackgroundColor(Color.MAGENTA);
+                }
+            }
+            else
+            {
+                //getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
     @Override
     protected void onResume()
     {
         super.onResume();
         accelerometer.register();
         gyroscope.register();
+        shakeManager.registerListener(mSensorListener, shakeManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
     }
     @Override
     protected void onPause()
@@ -310,6 +178,8 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
         super.onPause();
         accelerometer.unregister();
         gyroscope.unregister();
+        shakeManager.unregisterListener(mSensorListener);
+
     }
 
     @Override
@@ -317,8 +187,8 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
         this.player = player;
         if (!wasRestored) {
             ArrayList<String> arrayList = new ArrayList<String>();
-            arrayList.add("nOHkmBYuPK4");
-            arrayList.add("V70ruRzEGOI");
+            arrayList.add("qiYKqAPlTdk");
+            arrayList.add("AR-l4OBsPG0");
             arrayList.add("CcsUYu0PVxY");
             arrayList.add("RrDHrwLUtvk");
             arrayList.add("79DijItQXMM");
